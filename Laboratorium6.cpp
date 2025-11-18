456fce758cec49ed0a6b77e7ddd74101adeb1003
Kinga <kinga.janiak@student.put.poznan.pl>
2025-11-18 16:39:18 +0100

    18.11

--- Laboratorium6.cpp
+++ Laboratorium6.cpp
@@ -2,11 +2,84 @@
 //
 
 #include <iostream>
+using namespace std;
+
+//ZAD.1
+/*
+int suma(int a, int b);
+
+int a = 20;
+int b = 10;
+
+int suma(int a, int b) {
+
+	return a + b;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a = 1;
+	int b = 2;
+
+	suma(a, b);
+
+	cout << suma;
+
+	return 0;
 }
+*/
+
+//ZAD.2
+
+int tab[10];
+int rozmiar = 10;
+int usun();
+
+int main() {
+	int a;
+	
+	cout << "podaj 10 wartosci calkowitych ";
+
+	for (int i = 0; i < 10; i++) {
+		cin >> a;
+
+		tab[i] = a;
+
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << tab[i] << "  ";
+	}
+
+	usun();
+
+
+	return 0;
+}
+
+
+int usun() {
+
+	int x;
+
+	cout << " ktora pozycje w tabeli usunac?" << endl;
+	cin >> x;
+
+
+	for (int i = 0; i < 9; i++) {
+		if (x = i) {
+			break;
+		}
+		else {
+			cout << tab[i] << "  ";
+		}
+
+	}
+
+	return 0;
+
+}
+
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
 // Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
